31daf87a8de8cfe66417c29ce88a8298c699c518
Anil Acar <ostravba@gmail.com>
2023-08-13 12:43:52 +0200

    sidebar active class js and animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 const navLinks = document.querySelectorAll('nav ul li a')
 
-navLinks.forEach((item) => {
-  console.log(item)
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    const activeNavLink = document.querySelector('.active')
+    activeNavLink.classList.remove('active')
+    navLink.classList.add('active')
+
+    const activeSection = document.querySelector('main > section.active')
+    activeSection.classList.remove('active')
+
+    document
+      .querySelector(`main > section${navLink.getAttribute('href')}`)
+      .classList.add('active')
+  })
+
+  console.log(navLink.getAttribute('href'))
 })
